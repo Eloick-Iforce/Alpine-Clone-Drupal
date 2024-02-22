cd1f924a6211b029f290f9dd5c065d8faef0af95
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-02-22 13:13:18 +0100

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./web/themes/custom/alpine/templates/**/*.twig"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
